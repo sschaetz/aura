f81fbff3686b740382b917dc0e7babde6c08a761
sschaet <sschaet@gwdg.de>
2013-05-25 17:32:23 +0200

    Build Enhancements and first Unit test
    An additional CMake script allows simple inclusion of test cases.
    OpenCL is now merely the default configuration that can be overwritten.
    The first test case is still empty.

---
+++ test/backend/basic.cpp
@@ -0,0 +1,13 @@
+#define BOOST_TEST_MODULE backend.basic 
+
+#include <boost/test/unit_test.hpp>
+
+//#include <aura/backend.hpp>
+
+// basic
+// _____________________________________________________________________________
+
+BOOST_AUTO_TEST_CASE(basic)
+{
+}
+
