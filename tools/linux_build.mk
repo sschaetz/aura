d391a78e06a3914f8306a49c2b4a660f278f2e11
sschaet <sschaet@gwdg.de>
2013-05-27 16:46:13 +0200

    Build file (Linux) and config.hpp change
    To simplify building Aura in parallel, the config.hpp file is now not
    generated in the source directory but in the build directory. This is the
    correct location for a generated file anyways. An installation script would
    need to copy this file to the installation directory of the include files.

---
+++ tools/linux_build.mk
@@ -0,0 +1,33 @@
+# makefile to build both cuda and opencl versions of aura on linux
+#
+# run something like
+# 	make -j2 -f ./tools/linux_build.mk cuda opencl
+# to build cuda and opencl versions in parallel
+
+THIS_MAKEFILE_PATH:=$(word $(words $(MAKEFILE_LIST)),$(MAKEFILE_LIST))
+THIS_DIR:=$(shell cd $(dir $(THIS_MAKEFILE_PATH));pwd)
+
+cmake_cuda: 
+	mkdir -p /tmp/aura-cuda-build
+	cd /tmp/aura-cuda-build; \
+	cmake -DAURA_BACKEND=CUDA $(THIS_DIR)/../
+
+cuda: cmake_cuda 
+	cd /tmp/aura-cuda-build; \
+	$(MAKE) 
+
+cmake_opencl: 
+	mkdir -p /tmp/aura-opencl-build
+	cd /tmp/aura-opencl-build; \
+	cmake -DAURA_BACKEND=OPENCL $(THIS_DIR)/../
+
+opencl: cmake_opencl 
+	cd /tmp/aura-opencl-build; \
+	$(MAKE) 
+
+clean:
+	rm -rf /tmp/aura-opencl-build
+	rm -rf /tmp/aura-cuda-build
+
+all: cuda opencl
+
