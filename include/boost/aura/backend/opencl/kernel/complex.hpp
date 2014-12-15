d6146b749ecaef1e6005c2f8800c2f96959d78bd
sschaet <sschaet@gwdg.de>
2014-12-15 14:14:12 +0100

    removed static keyword for kernel functions

--- include/boost/aura/backend/opencl/kernel/complex.hpp
+++ include/boost/aura/backend/opencl/kernel/complex.hpp
@@ -4,43 +4,43 @@
 // single precision -----
 typedef float2 cfloat;
 
-static inline float crealf(cfloat x) 
+inline float crealf(cfloat x) 
 { 
 	return x.x; 
 }
 
-static inline float cimagf(cfloat x) 
+inline float cimagf(cfloat x) 
 { 
 	return x.y; 
 }
 
-static inline cfloat make_cfloat(float r, float i)
+inline cfloat make_cfloat(float r, float i)
 {
 	return (cfloat)(r, i);
 }
 
-static inline cfloat conjf(cfloat x)
+inline cfloat conjf(cfloat x)
 {
     return make_cfloat(x.x, -x.y);
 }
 
-static inline cfloat caddf(cfloat x, cfloat y)
+inline cfloat caddf(cfloat x, cfloat y)
 {
 	return make_cfloat(x.x+y.x, x.y+y.y);
 }
 
-static inline cfloat csubf(cfloat x, cfloat y)
+inline cfloat csubf(cfloat x, cfloat y)
 {
 	return make_cfloat(x.x-y.x, x.y-y.y);
 }
 
-static inline cfloat cmulf(cfloat x, cfloat y)
+inline cfloat cmulf(cfloat x, cfloat y)
 {
 	return make_cfloat(x.x * y.x - x.y * y.y, 
 			x.x*y.y + x.y*y.x);
 }
 
-static inline cfloat cdivf(cfloat x, cfloat y)
+inline cfloat cdivf(cfloat x, cfloat y)
 {
 	float n = 1.0f / (y.x*y.x + y.y*y.y);
 	float r = (x.x*y.x + x.y*y.y) * n;
@@ -48,7 +48,7 @@ static inline cfloat cdivf(cfloat x, cfloat y)
 	return make_cfloat(r, i);
 }
 
-static inline float cabsf(cfloat x)
+inline float cabsf(cfloat x)
 {
 	float rp = x.x;
 	float ip = x.y;
@@ -69,43 +69,43 @@ static inline float cabsf(cfloat x)
 // double precision -----
 typedef double2 cdouble;
 
-static inline double creal(cdouble x) 
+inline double creal(cdouble x) 
 { 
 	return x.x; 
 }
 
-static inline double cimag(cdouble x) 
+inline double cimag(cdouble x) 
 { 
 	return x.y; 
 }
 
-static inline cdouble make_cdouble(double r, double i)
+inline cdouble make_cdouble(double r, double i)
 {
 	return (cdouble)(r, i);
 }
 
-static inline cdouble conj(cdouble x)
+inline cdouble conj(cdouble x)
 {
 	return make_cdouble(x.x, -x.y);
 }
 
-static inline cdouble cadd(cdouble x, cdouble y)
+inline cdouble cadd(cdouble x, cdouble y)
 {
 	return make_cdouble(x.x+y.x, x.y+y.y);
 }
 
-static inline cdouble csub(cdouble x, cdouble y)
+inline cdouble csub(cdouble x, cdouble y)
 {
 	return make_cdouble(x.x-y.x, x.y-y.y);
 }
 
-static inline cdouble cmul(cdouble x, cdouble y)
+inline cdouble cmul(cdouble x, cdouble y)
 {
 	return make_cdouble(x.x * y.x - x.y * y.y, 
 			x.x*y.y + x.y*y.x);
 }
 
-static inline cdouble cdiv(cdouble x, cdouble y)
+inline cdouble cdiv(cdouble x, cdouble y)
 {
 	double n = 1.0f / (y.x*y.x + y.y*y.y);
 	double r = (x.x*y.x + x.y*y.y) * n;
@@ -113,7 +113,7 @@ static inline cdouble cdiv(cdouble x, cdouble y)
 	return make_cdouble(r, i);
 }
 
-static inline double cabs(cdouble x)
+inline double cabs(cdouble x)
 {	
 	double rp = x.x;
 	double ip = x.y;
@@ -131,13 +131,13 @@ static inline double cabs(cdouble x)
 }
 
 /// promotion
-static inline cdouble cfloat_to_cdouble(cfloat c)
+inline cdouble cfloat_to_cdouble(cfloat c)
 {
     return make_cdouble((double)crealf(c), (double)cimagf(c));
 }
 
 /// demotion
-static inline cfloat cdouble_to_cfloat(cdouble c) 
+inline cfloat cdouble_to_cfloat(cdouble c) 
 {
 	return make_cfloat((float)creal(c), (float)cimag(c));
 }
