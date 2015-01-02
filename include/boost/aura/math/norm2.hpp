1adb9e8d5b9c7ffca9a58dc2fe6075093eefeb96
sschaet <sschaet@gwdg.de>
2015-01-02 13:21:14 +0100

    modified norm2 experimental math infrastructure

--- include/boost/aura/math/norm2.hpp
+++ include/boost/aura/math/norm2.hpp
@@ -15,34 +15,16 @@ namespace math
 namespace detail
 {
 
-inline void norm2(device_ptr<float> src_ptr, const std::size_t size,
-		device_ptr<float> dst_ptr, feed& f)
+inline const char* norm2_kernel_name(device_ptr<float> src_ptr, 
+		device_ptr<float> dst_ptr)
 {
-	const char* kernel_string = R"aura_kernel(
-		#include <boost/aura/backend.hpp>
-		AURA_KERNEL
-		void norm2_float(AURA_GLOBAL float* src_ptr, 
-				AURA_GLOBAL float* dst_ptr) {}
-	)aura_kernel";	
-	backend::kernel k = src_ptr.get_device().load_from_string(
-			kernel_string, "norm2_float",
-			AURA_BACKEND_COMPILE_FLAGS);
-	invoke(k, size, args(src_ptr.get(), dst_ptr.get()), f);
+	return "norm2_float";	
 }
 
-inline void norm2(device_ptr<cfloat> src_ptr, const std::size_t size,
-		device_ptr<cfloat> dst_ptr, feed& f)
+inline const char* norm2_kernel_name(device_ptr<cfloat> src_ptr, 
+		device_ptr<cfloat> dst_ptr)
 {
-	const char* kernel_string = R"aura_kernel(
-		#include <boost/aura/backend.hpp>
-		AURA_KERNEL
-		void norm2_cfloat(AURA_GLOBAL cfloat* src_ptr, 
-				AURA_GLOBAL cfloat* dst_ptr) {} 
-	)aura_kernel";
-	backend::kernel k = src_ptr.get_device().load_from_string(
-			kernel_string, "norm2_cfloat",
-			AURA_BACKEND_COMPILE_FLAGS);
-	invoke(k, size, args(src_ptr.get(), dst_ptr.get()), f);
+	return "norm2_cfloat";	
 }
 
 }
@@ -51,9 +33,14 @@ template <typename DeviceRangeType>
 void norm2(DeviceRangeType& input_range, 
 		DeviceRangeType& output_range, feed& f)
 {
-	detail::norm2(aura::traits::begin(input_range),
-			aura::traits::size(input_range),
-			aura::traits::begin(output_range), f);
+	auto kernel_name = detail::norm2_kernel_name(
+			aura::traits::begin(input_range),
+			aura::traits::begin(output_range));
+	backend::kernel k = src_ptr.get_device().load_from_file(
+			"norm2_kernels.cc", kernel_name,
+			AURA_BACKEND_COMPILE_FLAGS);
+	invoke(k, size, args(aura::traits::begin(input_range), 
+				aura::traits::begin(output_range)), f);
 	return;
 }
 
---
+++ include/boost/aura/math/norm2_kernels.cpp
@@ -0,0 +1,12 @@
+#include <boost/aura/backend.hpp>
+
+AURA_KERNEL void norm2_float(AURA_GLOBAL float* src_ptr, 
+		AURA_GLOBAL float* dst_ptr)
+{
+}
+
+AURA_KERNEL void norm2_cfloat(AURA_GLOBAL cfloat* src_ptr, 
+		AURA_GLOBAL cfloat* dst_ptr) 
+{
+} 
+
