9c996e0fe07d4bb7d7cc51cbf2e3e589e2a9a810
Jakob Klosowski <jklosow@gwdg.de>
2015-04-17 16:08:11 +0200

    new atomic functions

--- include/boost/aura/backend/cuda/kernel/atomic.hpp
+++ include/boost/aura/backend/cuda/kernel/atomic.hpp
@@ -5,5 +5,21 @@ __device__ static __forceinline__ float atomic_addf(float* address, float val) {
 	return atomicAdd(address, val);
 }
 
+__device__ static __forceinline__ float atomic_maxf(float* address, float val) {
+
+	int* address_as_i = (int*) address;
+    int old = *address_as_i, assumed;
+
+    do {
+
+        assumed = old;
+        old = atomicCAS(address_as_i, assumed,
+            __float_as_int(fmaxf(val, __int_as_float(assumed))));
+
+    } while (assumed != old);
+
+    return __int_as_float(old);
+}
+
 #endif // AURA_BACKEND_CUDA_KERNEL_ATOMIC_HPP
 
--- include/boost/aura/backend/opencl/kernel/atomic.hpp
+++ include/boost/aura/backend/opencl/kernel/atomic.hpp
@@ -20,5 +20,22 @@ inline float atomic_addf(volatile __global float* address, float val) {
 	return oldvalue.f;
 }
 
+
+inline float atomic_maxf(volatile __global float* address, float val) {
+	union uif
+	{
+		unsigned int ui;
+		float f;
+	};
+
+	union uif newvalue;
+	union uif oldvalue;
+	do {
+		oldvalue.f = *address;
+		newvalue.f = oldvalue.f >= val ? oldvalue.f : val;
+	} while (atomic_cmpxchg((volatile __global unsigned int *)address,
+				oldvalue.ui, newvalue.ui) != oldvalue.ui);
+	return oldvalue.f;
+}
 #endif // AURA_BACKEND_OPENCL_KERNEL_ATOMIC_HPP
 
