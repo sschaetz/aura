c8681b12c7b5cea7af17a6e115cc6911f1cdf232
Christian Holme <cholme@gmx.com>
2015-04-23 14:58:40 +0200

    utility.hpp: moved tsizeof to meta/tsizeof.hpp
    Since it was implemented twice, tsizeof was removed from utility.hpp.
    It is already implemented in meta/tsizeof.hpp

--- include/boost/aura/utility.hpp
+++ include/boost/aura/utility.hpp
@@ -4,21 +4,6 @@ namespace boost
 {
   namespace aura
   {
-    // Calculates the total sizeof of all aguments
-    template<typename... Targs>
-      struct tsizeof;
-    template<typename T0>
-      struct tsizeof<T0>
-      {
-        enum ts : size_t {sz = sizeof(T0)};
-      };
-
-    template<typename T0, typename... Targs>
-      struct tsizeof<T0,Targs...>
-      {
-        enum ts : size_t {sz = sizeof(T0)+tsizeof<Targs...>::sz};
-      };
-    //----------------------------------------------
     // make_unique appears in gcc 4.9 XXX This should be obsolete then
     template<typename T, typename... Args>
       std::unique_ptr<T> make_unique(Args&&... args) {
