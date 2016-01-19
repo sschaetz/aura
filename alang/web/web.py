986e75382be7a0345f4b788dd122c1389b9a01a8
Sebastian Schaetz <sschaetz@butterflynetinc.com>
2016-01-18 22:10:11 -0500

    Implement alang ui round-trip

--- alang/web/web.py
+++ alang/web/web.py
@@ -1,6 +1,8 @@
 '''Alang web interface.'''
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import jsonify
 
 APP = Flask(__name__)
 
@@ -9,5 +11,14 @@ def index():
     '''Render index page.'''
     return render_template('index.html')
 
+@APP.route('/compile', methods=['POST'])
+def compile():
+    '''Compile the source code.'''
+    print("Compile method called!")
+    if request.method == 'POST':
+        print(request.form['code'])
+        return jsonify({'input': request.form['code']})
+
 if __name__ == '__main__':
+    APP.debug = True
     APP.run()
