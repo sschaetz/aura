6f9b0afbebec471e9e3b73101d09bbe46fdbb799
Sebastian Schaetz <sschaetz@butterflynetinc.com>
2016-05-16 17:14:02 -0400

    Fix manual build script

--- buildprocess/build.sh
+++ buildprocess/build.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-function build_and_test_target 
+function build_and_test_target() 
 {
-        BASEDIR=$(dirname "$0")
+        CURDIR=$(pwd)
+        BASEDIR=$(cd "$(dirname "$1")"; pwd)/$(basename "$1")
+        echo $BASEDIR
+        rm -rf /tmp/aura-build
         mkdir -p /tmp/aura-build
         cd /tmp/aura-build
         cmake $BASEDIR/../ -DAURA_BASE=$1
         make all -s -j8
         ctest
+        cd $CURDIR
 }
 
 build_and_test_target "METAL"
