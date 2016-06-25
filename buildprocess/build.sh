b49a75985e2c387332b4665fd458943715a4b144
Sebastian Schaetz <sschaetz@butterflynetinc.com>
2016-06-25 03:14:54 -0400

    Extend build script to work with local badge ciproxy.

--- buildprocess/build.sh
+++ buildprocess/build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -ev
-
 function build_and_test_target() 
 {
         CURDIR=$(pwd)
@@ -10,13 +8,21 @@ function build_and_test_target()
         rm -rf /tmp/aura-build
         mkdir -p /tmp/aura-build
         cd /tmp/aura-build
-        cmake $BASEDIR/../ -DAURA_BASE=$1
-        make all -s -j8
-        ctest
+        cmake $BASEDIR/../ -DAURA_BASE=$1 > /tmp/aura-build-$1.log 2>&1
+        CMAKE_RESULT=$?
+        make all -s -j8 >> /tmp/aura-build-$1.log 2>&1
+        MAKE_RESULT=$?
+        ctest >> /tmp/aura-build-$1.log 2>&1
+        CTEST_RESULT=$?
         cd $CURDIR
+        echo "CMAKE" $CMAKE_RESULT "MAKE" $MAKE_RESULT CTEST $CTEST_RESULT
+        curl --upload-file /tmp/aura-build-$1.log $4newbr?cmake_result=$CMAKE_RESULT\&make_result=$MAKE_RESULT\&ctest_result=$CTEST_RESULT\&branch=$2\&commitid=$3\&backend=$1\&machine=$(hostname)
 }
 
-build_and_test_target "CUDA"
-build_and_test_target "METAL"
-build_and_test_target "OPENCL"
+BRANCH=$(git branch | sed -n '/\* /s///p')
+COMMIT=$(git rev-parse HEAD)
 
+build_and_test_target "CUDA" $BRANCH $COMMIT $1
+build_and_test_target "METAL" $BRANCH $COMMIT $1
+build_and_test_target "OPENCL" $BRANCH $COMMIT $1
+rm -rf /tmp/aura-build
