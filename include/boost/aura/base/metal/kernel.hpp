17c9fdb04f8d2f88926395eb3b06fc89dae00663
Sebastian Schaetz <sschaetz@butterflynetinc.com>
2016-06-27 10:14:28 -0400

    Add Metal kernel

---
+++ include/boost/aura/base/metal/kernel.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <boost/aura/base/metal/safecall.hpp>
+#include <boost/aura/base/metal/library.hpp>
+
+namespace boost
+{
+namespace aura
+{
+namespace base_detail
+{
+namespace metal
+{
+
+class kernel
+{
+public:
+        /// Create kernel from library.
+        inline explicit kernel(const std::string& name, library& l)
+        {
+                NSString* kernel_name = @(name.c_str());
+                kernel_ =
+                        [l.get_base_library() newFunctionWithName:kernel_name];
+                AURA_METAL_CHECK_ERROR(kernel_);
+        }
+
+        /// Prevent copies.
+        kernel(const kernel&) = delete;
+        void operator=(const kernel&) = delete;
+
+        /// Destroy kernel.
+        inline ~kernel()
+        {
+                kernel_ = nil;
+        }
+
+private:
+        id<MTLFunction> kernel_;
+};
+
+} // namespace metal
+} // namespace base_detail
+} // namespace aura
+} // namespace boost
