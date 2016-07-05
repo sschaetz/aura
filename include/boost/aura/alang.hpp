2a72421dc57abf34d04f497ecc99dc702660c689
Sebastian Schaetz <sschaetz@butterflynetinc.com>
2016-07-05 15:00:10 -0400

    Finish alang header

--- include/boost/aura/alang.hpp
+++ include/boost/aura/alang.hpp
@@ -1,4 +1,3 @@
-/*
 #pragma once
 
 namespace boost
@@ -6,12 +5,11 @@ namespace boost
 namespace aura
 {
 
-const char* alang_header = R"(
-*/
+const char* alang_header_global = R"(
 #define M_PI 3.14159265358979323846
+)";
 
-#if defined AURA_BASE_OPENCL // ---------------------------
-
+const char* alang_header_opencl = R"(
 #define AURA_KERNEL __kernel
 #define AURA_CONSTANT __constant
 #define AURA_DEVMEM __global
@@ -35,9 +33,9 @@ const char* alang_header = R"(
 #define AURA_BUNDLE_SIZE_0 get_local_size(0)
 #define AURA_BUNDLE_SIZE_1 get_local_size(1)
 #define AURA_BUNDLE_SIZE_2 get_local_size(2)
+)";
 
-#elif defined AURA_BASE_METAL // ---------------------------
-
+const char* alang_header_metal = R"(
 #include <metal_stdlib>
 #include <metal_compute>
 #include <metal_math>
@@ -68,37 +66,34 @@ using namespace metal;
 #define AURA_BUNDLE_SIZE_0 aura_bundle_size.x
 #define AURA_BUNDLE_SIZE_1 aura_bundle_size.y
 #define AURA_BUNDLE_SIZE_2 aura_bundle_size.z
+)";
 
-#elif defined AURA_BASE_CUDA // ----------------------------
 
+const char* alang_header_cuda = R"(
 #define AURA_KERNEL extern "C" __global__
 #define AURA_CONSTANT
 #define AURA_DEVMEM
-#endif
 
 #define AURA_MESH_ID_ARG
-#define AURA_MESH_ID_0 blockDim.x *
-#define AURA_MESH_ID_1 get_global_id(1)
-#define AURA_MESH_ID_2 get_global_id(2)
+#define AURA_MESH_ID_0 blockIdx.x * blockDim.x + threadIdx.x
+#define AURA_MESH_ID_1 blockIdx.y * blockDim.y + threadIdx.y
+#define AURA_MESH_ID_2 blockIdx.z * blockDim.z + threadIdx.z
 
 #define AURA_MESH_SIZE_ARG
-#define AURA_MESH_SIZE_0 get_global_size(0)
-#define AURA_MESH_SIZE_1 get_global_size(1)
-#define AURA_MESH_SIZE_2 get_global_size(2)
+#define AURA_MESH_SIZE_0 gridDim.x * blockDim.x
+#define AURA_MESH_SIZE_1 gridDim.y * blockDim.y
+#define AURA_MESH_SIZE_2 gridDim.z * blockDim.z
 
 #define AURA_BUNDLE_ID_ARG
-#define AURA_BUNDLE_ID_0 get_local_id(0)
-#define AURA_BUNDLE_ID_1 get_local_id(1)
-#define AURA_BUNDLE_ID_2 get_local_id(2)
+#define AURA_BUNDLE_ID_0 threadId.x
+#define AURA_BUNDLE_ID_1 threadId.y
+#define AURA_BUNDLE_ID_2 threadId.z
 
 #define AURA_BUNDLE_SIZE_ARG
-#define AURA_BUNDLE_SIZE_0 get_local_size(0)
-#define AURA_BUNDLE_SIZE_1 get_local_size(1)
-#define AURA_BUNDLE_SIZE_2 get_local_size(2)
-
-/*
+#define AURA_BUNDLE_SIZE_0 blockDim.x
+#define AURA_BUNDLE_SIZE_1 blockDim.y
+#define AURA_BUNDLE_SIZE_2 blockDim.z
 )";
 
 } // namespace aura
 } // namespace boost
-*/
