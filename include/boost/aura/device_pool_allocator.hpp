ef61550b091a1b9f44e3a85b524fdf4643a4cdd2
Sebastian Schaetz <sschaetz@butterflynetinc.com>
2017-03-28 14:22:42 +0200

    Add device pool allocator (WIP)

---
+++ include/boost/aura/device_pool_allocator.hpp
@@ -0,0 +1,171 @@
+#pragma once
+
+#include <boost/core/ignore_unused.hpp>
+
+#include <boost/aura/device.hpp>
+#include <boost/aura/device_ptr.hpp>
+
+#include <map>
+#include <vector>
+
+namespace boost
+{
+namespace aura
+{
+
+/// General allocator for device memory.
+template <class T>
+struct device_pool_allocator
+{
+        using value_type = T;
+        using pointer = device_ptr<T>;
+        using const_pointer = const device_ptr<T>;
+
+        /// Construct empty allocator.
+        device_pool_allocator()
+        {}
+
+        /// Construct allocator.
+        /// @param d Device.
+        /// @param max_elements Maximum number of elements this pool can hold.
+        device_pool_allocator(device& d,
+                const std::size_t& max_elements = 10 * 1024 * 1024 / sizeof(T))
+                : device_(&d)
+                , max_elements_(max_elements)
+        {}
+
+        /// Copy construct allocator.
+        template <class U>
+        device_pool_allocator(const device_pool_allocator<U>& other)
+                : device_(other.device_)
+                , max_elements_(other.max_elements_)
+        {}
+
+        /// Move construct allocator.
+        template <class U>
+        device_pool_allocator(device_pool_allocator<U>&& other)
+                : device_(other.device_)
+                , max_elements_(other.max_elements_)
+        {
+                other.device_ = nullptr;
+                other.max_elements_ = 0;
+        }
+
+        ~device_pool_allocator()
+        {
+                assert(in_use_memory_.size() == 0);
+                // Allow no elements in the object and purge.
+                max_elements_= 0;
+                purge_();
+        }
+
+        /// Allocate memory.
+        pointer allocate(std::size_t n)
+        {
+                assert(device_);
+                pointer ptr;
+                // Check if we have available memory of that size.
+                auto it = available_memory_.find(n);
+
+                if (it != available_memory_.end())
+                {
+                        ptr = it->second.back();
+
+                        // Add to in_use.
+                        in_use_memory_[ptr] = it->first;
+
+                        // Erase from free.
+                        if (it->second.size() == 1)
+                        {
+                                // Remove this size entirely.
+                                available_memory_.erase(it);
+                        }
+                        else
+                        {
+                                // Only remove this one pointer from size.
+                                it->second.pop_back();
+                        }
+                }
+                else
+                {
+                        auto ptr = device_malloc<T>(n, *device_);
+                        num_elements_ += n;
+                        in_use_memory_[ptr] = n;
+                }
+                return ptr;
+        }
+
+        /// Deallocate memory.
+        void deallocate(pointer& p, std::size_t n)
+        {
+                assert(device_);
+
+                auto it = in_use_memory_.find(p);
+
+                // in_use_memory_ must contain this pointer.
+                if (it != in_use_memory_.end())
+                {
+                        assert(it->second == n);
+                        auto it2 = available_memory_.find(n);
+                        if (it2 != available_memory_.end())
+                        {
+                                // Add to existing vector.
+                                it2->second.push_back(it->first);
+                        }
+                        else
+                        {
+                                // Create new vector.
+                                available_memory_[it->second] = {it->first};
+                        }
+                        in_use_memory_.erase(it);
+                }
+                else
+                {
+                        assert(false);
+                }
+        }
+
+private:
+        /// Methos used to purge the allocator if it has grown too big.
+        void purge_()
+        {
+                /*
+                if (num_elements_ >= max_elements_)
+                {
+                        for (auto ptrs : available_memory_)
+                        {
+                                for (auto ptr : ptrs->second)
+                                {
+                                        device_free(ptr);
+                                        num_elements_ -= ptrs->first();
+                                }
+                        }
+
+                }
+                */
+        }
+
+        /// A single allocation is stored as it's size and a pointer.
+        using available_storage_t = std::tuple<std::size_t, pointer>;
+
+        /// Device we allocate memory from.
+        device* device_;
+
+        /// Flag that indicates if allocator is initialized or not.
+        bool initialized_ { false };
+
+        /// Maximum number of elements this allocator should hold.
+        std::size_t max_elements_;
+
+        /// Num elements held
+        std::size_t num_elements_ = { 0 };
+
+        /// List of free storage elements.
+        std::map<std::size_t, std::vector<pointer>> available_memory_;
+
+        /// List of in-use storage elements.
+        std::map<pointer, std::size_t> in_use_memory_;
+};
+
+} // namespace aura
+} // namespace boost
--- test/device_allocator.cpp
+++ test/device_allocator.cpp
@@ -2,6 +2,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <boost/aura/device_allocator.hpp>
+#include <boost/aura/device_pool_allocator.hpp>
 #include <boost/aura/environment.hpp>
 
 // _____________________________________________________________________________
@@ -13,6 +14,26 @@ BOOST_AUTO_TEST_CASE(basic_allocator)
                 boost::aura::device d(AURA_UNIT_TEST_DEVICE);
                 d.allocation_tracker.activate();
                 boost::aura::device_allocator<float> a(d);
+                BOOST_CHECK(d.allocation_tracker.count_active() == 0);
+                {
+                        auto ptr = a.allocate(1024);
+                        BOOST_CHECK(d.allocation_tracker.count_active() == 1);
+                        a.deallocate(ptr, 1024);
+                }
+                BOOST_CHECK(d.allocation_tracker.count_active() == 0);
+                BOOST_CHECK(d.allocation_tracker.count_old() == 1);
+
+        }
+        boost::aura::finalize();
+}
+
+BOOST_AUTO_TEST_CASE(pool_allocator)
+{
+        boost::aura::initialize();
+        {
+                boost::aura::device d(AURA_UNIT_TEST_DEVICE);
+                d.allocation_tracker.activate();
+                boost::aura::device_pool_allocator<float> a(d);
                 BOOST_CHECK(d.allocation_tracker.count_active() == 0);
                 {
                         auto ptr = a.allocate(1024);
